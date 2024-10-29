2afa974ffdd10dbe11b5f2864c058d696243a5b2
Furkan Abay <furkan_abay@hotmail.com>
2024-10-29 23:17:42 +0100

    feat: cleanup on isUserManager to make less request and removed unused types

--- utils/isUserManager.ts
+++ utils/isUserManager.ts
@@ -13,45 +13,24 @@ export async function userIsManager(accessToken: string) {
     return null
   }
 
-  const userStoreRole = await getUserStoreRole(data.user.id);
-  const storeId = await getUserStore(data.user.id)
+  const userStoreData = await getUserStoreData(data.user.id);
 
   const managerObject = {
-    isManager: userStoreRole === "manager" ? true : false,
-    store: storeId
+    isManager: userStoreData?.store_role === "manager" ? true : false,
+    store: userStoreData?.store
   }
 
   return managerObject
 }
 
-type UserStoreRole = {
-  store_role: Database["public"]["Tables"]["employees"]["Row"]["store_role"]
-}
-
-export async function getUserStoreRole(UUID: string) {
+export async function getUserStoreData(UUID: string) {
   const { data, error } = await supabase
     .from('employees')
-    .select('store_role')
+    .select('store_role, store')
     .eq('id', UUID)
     .single();
 
   if (error || !data) return null
 
-  return data.store_role
-}
-
-type UserStore = {
-  store: Database["public"]["Tables"]["employees"]["Row"]["store"]
-}
-
-export async function getUserStore(UUID: string) {
-  const { data, error } = await supabase
-    .from('employees')
-    .select('store')
-    .eq('id', UUID)
-    .single();
-
-  if (error || !data) return null
-
-  return data.store
+  return data
 }
